8ee6238bc92aa5f4e48a0e2546dbaa9f86ebb190
PRAMOD-SAMSON <pramod.maben@gmail.com>
2026-07-20 14:16:10 +0530

    Enlarge module hero images further (1fr 2.2fr)

--- kodeit-next/components/ModuleHero.jsx
+++ kodeit-next/components/ModuleHero.jsx
@@ -63,7 +63,7 @@ export default function ModuleHero({
             height={716}
             priority
             quality={90}
-            sizes="(max-width: 980px) 100vw, 720px"
+            sizes="(max-width: 980px) 100vw, 820px"
           />
         </div>
       </div>
